89596600c32721a271e71cd13d87ab8e9b59dcd8
Tadiwa Leon Muguta <leonmuguta7@gmail.com>
2022-02-22 14:14:28 +0200

    Url shorten function but ran into some errors

--- src/UrlSection.js
+++ src/UrlSection.js
@@ -3,13 +3,32 @@ import { useState } from "react";
 const UrlSection = () => {
 
     const [url, setUrl] = useState("");
+    const [isLoading, setIsLoading] = useState(false);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        fetch('https://api-ssl.bitly.com/v4/shorten', {
+            method: 'POST',
+            headers: {
+                'Authorization': 'Bearer {b41b45e401ba987fae260afe2777bb230c7ef5e8}',
+                'Content-Type': 'application/json'
+            },
+            body: { "long_url": "https://dev.bitly.com/", "domain": "bit.ly", "group_guid": "Ba1bc23dE4F" }
+        }).then((res) => {
+            res.json()
+            console.log(res);
+            setIsLoading(false);
+        })
+    }
 
     return ( 
         <div className="paste-section">
             <h1>Paste in your URL below</h1>
-            <form action="" method="post">
-                <input type="text" className="urlForm" value={url} onChange={(e) => setUrl(e.target.value)} placeholder="Enter url"/>
-                <button className="button">Generate</button>
+            <form onSubmit={handleSubmit}>
+                <input type="text" className="urlForm" value={url} onChange={(e) => setUrl(e.target.value)} placeholder="Enter url" required/>
+                {!isLoading && <button className="button">Generate</button>}
+                {isLoading && <button className="button" disabled>Generating...</button>}
             </form>
         </div>
     );
